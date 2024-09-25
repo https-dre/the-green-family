c2d5c2cb6d3c2a1310bcc2e0396f85991b46ae18
http.dre <diaso.andre@outlook.com>
2024-09-25 18:38:24 -0300

    add BottomTab router

---
+++ BottomTabRouter.jsx
@@ -0,0 +1,16 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { HomeScreen } from "./screens/HomeScreen"
+
+const Tab = createBottomTabNavigator();
+
+export function BottomTabsRouter() {
+    return (
+        <Tab.Navigator screenOptions={{headerShown: false}}>
+            <Tab.Screen name="Home" 
+                component={HomeScreen}
+                screenOptions={{}}    
+            />
+        </Tab.Navigator>
+    )
+}

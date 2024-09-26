1dc2ac5b6763b4c0444360d6489b8fec7caa0ab1
http.dre <diaso.andre@outlook.com>
2024-09-26 20:07:12 -0300

    Adiciona HomeIsland a tela home inicial

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,9 +1,32 @@
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, View, StyleSheet, ScrollView, TouchableWithoutFeedback, Keyboard } from "react-native";
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import { HomeIsland } from "../components/HomeIsland"
+import { BebeInfo } from "../components/BebeInfo"
 
 export function HomeScreen() {
     return (
-        <SafeAreaView>
-            <Text>Temos uma Home!</Text>
+        <SafeAreaView style={styles.container}>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+                <ScrollView style={[styles.container, { padding: 15 }]}>
+                    <View style={{ flexDirection: "row", justifyContent: "space-between", marginVertical: 15 }}>
+                        <Icon name="moon-o" size={25} color="black" />
+                        <Icon name="user-circle-o" size={30} color="black" />
+                    </View>
+
+                    <HomeIsland>
+                        <BebeInfo nomeBebe="Gabriel" idade="1 ano, 5 meses" />
+                    </HomeIsland>
+
+                </ScrollView>
+            </TouchableWithoutFeedback>
+
         </SafeAreaView>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "white"
+    }
+})

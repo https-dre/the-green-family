f6691f6e55299b95687a20a500f3edc038e29bae
http.dre <diaso.andre@outlook.com>
2024-10-02 18:07:52 -0300

    adiciona dependências para conseguir usar configurações de ambiente no react-native e instala o react-native-maps

--- babel.config.js
+++ babel.config.js
@@ -4,3 +4,13 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
   };
 };
+
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['module:react-native-dotenv', {
+      moduleName: '@env',
+      path: '.env',
+    }]
+  ],
+};

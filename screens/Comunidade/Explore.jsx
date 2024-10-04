3ae44d9d69c9e41f08985e70a76f9498762abbf7
http.dre <diaso.andre@outlook.com>
2024-10-04 15:14:25 -0300

    adiciona componentes e estilos no 'Explore' e no 'YourGroups'

--- screens/Comunidade/Explore.jsx
+++ screens/Comunidade/Explore.jsx
@@ -1,9 +1,61 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity, StyleSheet, ScrollView } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome6"
+
+import { Grupo } from "../../components/Grupo";
+
+const groups = [
+    {
+        name: "Itens Escolares",
+        desc: "Comunidade para doações de livros e uniformes escolares da escola.",
+        count: "2.9k"
+    }
+]
+
+const handleGroups = (array) => {
+    let components = []
+    array.forEach((e, index) => {
+        components.push(
+            <Grupo 
+                key={index}  // Adicione a propriedade key aqui
+                groupName={e.name} 
+                desc={e.desc} 
+                isFavorited={e.isFavorited} 
+                count={e.count} 
+            />
+        )
+    })
+
+    return components
+}
 
 export const Explore = () => {
+    const groupList = handleGroups(groups)
     return (
-        <View>
-            <Text style={{fontSize: 50}}>Explore</Text>
-        </View>
+        <ScrollView style={{ backgroundColor: "white", flex: 1 }}>
+            <View style={styles.explorePopulares}>
+                <View>
+                    <Text style={{ fontSize: 28, fontWeight: "bold" }}>Populares</Text>
+                    <Text>Os grupos mais ativos da Comunidade</Text>
+                </View>
+                <TouchableOpacity>
+                    <Icon name="angle-right" color="black" size={30} />
+                </TouchableOpacity>
+            </View>
+             {/* Grupos */}
+             <View>
+                {groupList}
+             </View>
+
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    explorePopulares: { 
+        flexDirection: "row", 
+        alignItems: "center", 
+        justifyContent: "space-between", 
+        paddingHorizontal: 20,
+        marginVertical: 25 
+    }
+})
--- screens/Comunidade/YourGroups.jsx
+++ screens/Comunidade/YourGroups.jsx
@@ -1,9 +1,55 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity, StyleSheet, ScrollView } from "react-native";
+import { Grupo } from "../../components/Grupo";
+
+const groups = [
+    {
+        name: "Apoio para pais de primeira viagem",
+        desc: "Comunidade que apoia pais de primeira viagem e visa um futuro melhor para os filhos.",
+        count: 1.2
+    }
+]
+
+const handleGroups = (array) => {
+    let components = []
+    array.forEach((e, index) => {
+        components.push(
+            <Grupo 
+                key={index}  // Adicione a propriedade key aqui
+                groupName={e.name} 
+                desc={e.desc} 
+                isFavorited={e.isFavorited} 
+                count={e.count} 
+            />
+        )
+    })
+
+    return components
+}
 
 export const YourGroups = () => {
+    const groupList = handleGroups(groups)
     return (
-        <View>
-            <Text style={{fontSize: 50}}>YourGroups</Text>
-        </View>
+        <ScrollView style={{ backgroundColor: "white", flex: 1 }}>
+            <View style={styles.explorePopulares}>
+                <View>
+                    <Text style={{ fontSize: 28, fontWeight: "bold" }}>Grupos que você entrou</Text>
+                </View>
+            </View>
+             {/* Grupos */}
+             <View>
+                {groupList}
+             </View>
+
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    explorePopulares: { 
+        flexDirection: "row", 
+        alignItems: "center", 
+        justifyContent: "space-between", 
+        paddingHorizontal: 20,
+        marginVertical: 25 
+    }
+})
